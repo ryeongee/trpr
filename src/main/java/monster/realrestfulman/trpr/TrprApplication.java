3cca45e9fe1302123e1e114063209966ecf89316
ryeongee <ryeongee24@gmail.com>
2023-12-11 23:48:28 +0900

    Initial commit

---
+++ src/main/java/monster/realrestfulman/trpr/TrprApplication.java
@@ -0,0 +1,13 @@
+package monster.realrestfulman.trpr;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrprApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TrprApplication.class, args);
+	}
+
+}
---
+++ src/test/java/monster/realrestfulman/trpr/TrprApplicationTests.java
@@ -0,0 +1,13 @@
+package monster.realrestfulman.trpr;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TrprApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
